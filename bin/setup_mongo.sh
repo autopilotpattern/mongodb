9de28a87d49742e554cb646bc73f612ade9a31e7
geek <wpreul@gmail.com>
2017-03-14 14:46:21 -0500

    Updates after feedback

--- bin/setup_mongo.sh
+++ bin/setup_mongo.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo "Start MongoDB without access control."
-mongod --port 27017 &
-while ! nc -z 127.0.0.1 27017; do sleep 1; done
+set -e
 
+echo "Start MongoDB without access control and only for local connections"
+mongod --fork --bind_ip 127.0.0.1 --logpath /dev/stdout
 
 echo "Create the user administrator."
+# The createUser will error if the user already exists.
 mongo admin --eval "db.createUser({ user: '${MONGO_USER}', pwd: '${MONGO_PASSWORD}', roles: [ { role: 'dbAdminAnyDatabase', db: 'admin' }, { role: 'clusterAdmin', db: 'admin' } ] });"
 
 
